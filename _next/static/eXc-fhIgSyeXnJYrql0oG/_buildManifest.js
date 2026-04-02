1ef8c1436aa4a4b83af3b2a97705c141d0cddaad
beingvikrant2000 <beingvikrant2000@gmail.com>
2026-04-02 22:42:23 +0200

    Updates

---
+++ _next/static/eXc-fhIgSyeXnJYrql0oG/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/lesspay//_next/:path+",
+        "destination": "/lesspay/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
